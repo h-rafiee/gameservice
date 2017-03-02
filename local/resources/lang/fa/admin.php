cb3c5984790396d7867fbe8e4fd73bf972521810
Hossein Raffiee <h.rafiee91@gmail.com>
2017-03-02 11:26:32 +0330

    [ * add ] lang fa

---
+++ local/resources/lang/fa/admin.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'header_title' => 'پنل مدیریت پلک',
+    'back_to_website' => 'بازگش به وب سایت',
+    'dashboard' => 'پیشخوان',
+    'members' => 'اعضای سایت',
+    'administrators' => 'مدیران ارشد',
+    'administrator' => 'مدیر ارشد',
+    'users' => 'کاربران',
+    'user' => 'کاربر',
+    'applications'=>'برنامه ها',
+    'achievements'=>'دستاوردها',
+    'confirm_delete' => 'آیا از حذف [ :val ] مطمئن هستید ؟',
+    'non_item_found' => 'موردی یافت نشد.',
+];
