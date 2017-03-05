34d66125e55a43e993964fb24522fde1198c213b
Hossein Raffiee <h.rafiee91@gmail.com>
2017-03-05 11:33:16 +0330

    [ * update ] Passport added to project

--- local/routes/api.php
+++ local/routes/api.php
@@ -13,6 +13,61 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
+Route::post('get_access',function(Request $request){
+
+    $http = new GuzzleHttp\Client;
+
+    $response = $http->post(url('oauth/token'), [
+        'form_params' => [
+            'grant_type'=>'client_credentials',
+            'client_id' => $request->get('client_id'),
+            'client_secret' => $request->get('client_secret'),
+        ],
+    ]);
+
+    return json_decode((string) $response->getBody(), true);
+});
+
+Route::post('authentication',function(Request $request){
+    $http = new GuzzleHttp\Client;
+
+    $response = $http->post(url('oauth/token'), [
+        'form_params' => [
+            'grant_type' => 'password',
+            'client_id' => $request->get('client_id'),
+            'client_secret' => $request->get('client_secret'),
+            'username' => $request->get('username'),
+            'password' => $request->get('password'),
+            'scope' => '',
+        ],
+    ]);
+
+    return json_decode((string) $response->getBody(), true);
+});
+
+Route::post('refresh_token',function(Request $request){
+    $http = new GuzzleHttp\Client;
+
+    $response = $http->post(url('oauth/token'), [
+        'form_params' => [
+            'grant_type' => 'refresh_token',
+            'refresh_token' => $request->get('refresh_token'),
+            'client_id' => $request->get('client_id'),
+            'client_secret' => $request->get('client_secret'),
+            'scope' => '',
+        ],
+    ]);
+
+    return json_decode((string) $response->getBody(), true);
+});
+
+
+Route::group(['namespace'=>'Api'],function() {
+    Route::group(['middleware' => 'auth:api'], function () {
+
+    });
+
+    Route::group(['middleware'=>'client_credentials'],function(){
+
+    });
+});
